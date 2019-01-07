66fdda42f1c4bd07ffc4b2e70f99705e49baebe8
Michał Szewczak <michasacuer3@gmail.com>
2019-01-07 22:42:01 +0100

    Farm and context refactor

--- Farm_ASPCore_Webapi/Models/Farm.cs
+++ Farm_ASPCore_Webapi/Models/Farm.cs
@@ -10,35 +10,34 @@ namespace Farm_ASPCore_Webapi.Models
     {
         [Key]
         public int Id { get; set; } = 1;
-        public string Name { get; set; } = "farma";
+        public string Name { get; set; }
 
         public virtual List<Stable> Stables { get; set; }
         public virtual List<Machine> Machines { get; set; }
         public virtual List<Worker> Workers { get; set; }
         public virtual List<Cultivation> Cultivations { get; set; }
-        //public virtual List<Driver> Drivers { get; set; }
-        //public virtual List<Farmer> Farmers { get; set; }
-        private string state;
 
         public static Farm GetFarm() => farm;
 
-        private static readonly Farm farm = new Farm();
-        private Farm() { }
-        
         public void saveState(string state)
         {
             this.state = state;
         }
+
         public Memento save()
         {
             return new Memento(state);
         }
+
         public void restore(Memento m)
         {
             state = m.getState();
             Console.WriteLine("Powrot do wersji poprzedniej");
             Console.WriteLine(state);
         }
-        
+
+        private string state;
+        private Farm() { }
+        private static readonly Farm farm = new Farm { Id = 1, Name = "Farm" };
     }
 }
