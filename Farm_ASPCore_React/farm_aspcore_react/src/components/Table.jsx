659ca7b4b419691a8bea0080b148d931c9c0d1b5
Morragan <sznurek05@gmail.com>
2019-01-08 14:44:30 +0100

    react cleared

---
+++ Farm_ASPCore_React/farm_aspcore_react/src/components/Table.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Table.css";
+
+class Table extends Component {
+  state = {};
+  render() {
+    return <div className="table">{}</div>;
+  }
+}
+
+export default Table;
---
+++ Farm_ASPCore_React/farm_aspcore_react/src/components/TableRow.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./TableRow.css";
+
+class TableRow extends Component {
+  state = {};
+  render() {
+    return <div className="table-row">{}</div>;
+  }
+}
+
+export default TableRow;
