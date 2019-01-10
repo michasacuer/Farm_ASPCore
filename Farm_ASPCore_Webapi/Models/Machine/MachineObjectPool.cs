1b7d2e09d35a5ab563b81a870e059a8f559c74bb
Morragan <sznurek05@gmail.com>
2019-01-10 02:32:04 +0100

    Object pool finished for today

--- Farm_ASPCore_Webapi/Models/Machine/MachineObjectPool.cs
+++ Farm_ASPCore_Webapi/Models/Machine/MachineObjectPool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,22 @@ namespace Farm_ASPCore_Webapi.Models
 {
     public class MachineObjectPool
     {
+        public MachineObjectPool Instance { get; } = new MachineObjectPool();
+        private readonly List<Machine> pool = new List<Machine>() { new Machine(), new Machine(), new Machine(), new Machine(), new Machine(), new Machine(), new Machine(), new Machine(), new Machine(), new Machine() };
+        private readonly int maxPoolSize = 10;
+
+        private MachineObjectPool() { }
+        public Machine AcquireMachine()
+        {
+            if (pool.Count <= 0) throw new Exception("Pool empty");
+            Machine reusable = pool.First();
+            pool.Remove(pool.First());
+            return reusable;
+        }
+        public void ReleaseMachine(Machine reusable)
+        {
+            if (pool.Count >= maxPoolSize) throw new Exception("Pool full, PANIC");
+            pool.Add(reusable);
+        }
     }
 }
