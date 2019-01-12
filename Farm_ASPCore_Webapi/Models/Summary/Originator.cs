8ef4a1ea09d980999e08173e603baf4969b8911d
Michał Szewczak <michasacuer3@gmail.com>
2019-01-12 23:40:48 +0100

    MEMENTO MOCKUP

---
+++ Farm_ASPCore_Webapi/Models/Summary/Originator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Farm_ASPCore_Webapi.Models.Summary
+{
+    public class Originator
+    {
+        public void SetState(Summary state) => this.state = state;
+        public Summary GetState() => state;
+        public Memento Save() => new Memento(state);
+        public void GetStateFromMemento(Memento memento) => state = memento.GetState();
+
+        private Summary state;
+    }
+}
