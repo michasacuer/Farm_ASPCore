a25030d5d31418ddd67617af96c46a4948cc4ed3
Michał Szewczak <michasacuer3@gmail.com>
2019-01-13 12:05:42 +0100

    refactor

---
+++ Farm_ASPCore_Webapi/Controllers/MachineController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Farm_ASPCore_Webapi.Models;
+
+namespace Farm_ASPCore_Webapi.Controllers
+{
+    [Route("api/Machine")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        private readonly FarmDbContext _context;
+
+        public MachineController(FarmDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Machine
+        [HttpGet]
+        public IEnumerable<Machine> GetMachines()
+        {
+            return _context.Machines;
+        }
+
+        // GET: api/Machine/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMachine([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var machine = await _context.Machines.FindAsync(id);
+
+            if (machine == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(machine);
+        }
+
+        // PUT: api/Machine/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMachine([FromRoute] int id, [FromBody] Machine machine)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != machine.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(machine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MachineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Machine
+        [HttpPost]
+        public async Task<IActionResult> PostMachine([FromBody] Machine machine)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Machines.Add(machine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMachine", new { id = machine.Id }, machine);
+        }
+
+        // DELETE: api/Machine/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMachine([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var machine = await _context.Machines.FindAsync(id);
+            if (machine == null)
+            {
+                return NotFound();
+            }
+
+            _context.Machines.Remove(machine);
+            await _context.SaveChangesAsync();
+
+            return Ok(machine);
+        }
+
+        private bool MachineExists(int id)
+        {
+            return _context.Machines.Any(e => e.Id == id);
+        }
+    }
+}
